7280bcac64b9e6042e47d9ea8592b5abd110f68a
Trần Ngọc Tiến <144195318+WalterGoldenOwl@users.noreply.github.com>
2023-09-08 11:22:12 +0700

    update home page

---
+++ Flutter/flutter_gallery_app/lib/src/data/models/gallery_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GalleryItem {
+  final IconData icon;
+  final String title;
+  final String subTitle;
+
+  GalleryItem({
+    required this.icon,
+    required this.title,
+    required this.subTitle,
+  });
+}
