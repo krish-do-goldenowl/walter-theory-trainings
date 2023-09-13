89520bffa61536547d82ecc0099f2b932f95d390
Trần Ngọc Tiến <144195318+WalterGoldenOwl@users.noreply.github.com>
2023-09-13 10:52:13 +0700

    update setup firebase

--- Flutter/flutter_gallery_app/lib/main.dart
+++ Flutter/flutter_gallery_app/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gallery_app/firebase_options.dart';
 import 'package:flutter_gallery_app/src/core/config/app_router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
